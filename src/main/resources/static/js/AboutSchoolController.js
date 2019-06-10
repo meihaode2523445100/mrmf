fefad6f82047161b0f39c6345480e2da66d66106
zhang <zhangyi1661@outlook.com>
2019-06-10 22:10:39 +0800

    zy005,导航栏功能完结

--- src/main/resources/static/js/AboutSchoolController.js
+++ src/main/resources/static/js/AboutSchoolController.js
@@ -28,11 +28,19 @@ function loadschoolpro() {
             success: function (data) {
                 if (data.code == 1) {
                     var info = data.data;
-                    $(".MainContent").empty();
+                    $(".RightSidebar").empty();
                     $(info).each(function (){
-                        var html = ' <p style="padding-bottom:20px;"><img src="../images/Pic20.jpg" /></p>';
-                        html +=    '  <p style="font-size:14px;">'+this.sp_introduction+'</p><br>';
-                        $(".MainContent").append($(html))
+                        var html = ' <div class="current">';
+                        html += ' <h1>学校简介<em>School Profile</em></h1>';
+                        html += ' <dl><a href="#">首页</a> > <a href="#">学校概况</a> > <span>学校简介</span></dl>';
+                        html += '  </div>';
+                        html += '  <div class="MainContent">';
+                        html += '      <p style="padding-bottom:20px;"><img src="../images/Pic20.jpg" /></p>';
+                        html += '      <p style="font-size:14px;">'+this.sp_introduction+'</p><br>';
+                        html += '  <div class="space20"></div>';
+                        html += ' </div>';
+                        $(".RightSidebar").append($(html))
+
                     })
                 }
             }
@@ -56,11 +64,20 @@ function loadAboutSchool(id) {
                     success: function (data) {
                         if (data.code == 1) {
                             var info = data.data;
-                            $(".MainContent").empty();
+                            $(".RightSidebar").empty();
                             $(info).each(function (){
-                                var html = ' <p style="padding-bottom:20px;"><img src="../images/Pic20.jpg" /></p>';
-                                html +=    '  <p style="font-size:14px;">'+this.sp_introduction+'</p><br>';
-                                $(".MainContent").append($(html))
+                                var html = ' <div class="current">';
+                                html += ' <h1>校长致辞<em>School Master</em></h1>';
+                                html += ' <dl><a href="#">首页</a> > <a href="#">学校概况</a> > <span>校长致辞</span></dl>';
+                                html += '  </div>';
+                                html += '  <div class="MainContent">';
+                                html += '     <p style="padding-bottom:20px;"><img src="../images/Pic20.jpg" /></p>';
+                                html += '        <p style="font-size:14px;">'+this.sm_time+'</p>';
+                                html += '       <p style="font-size:14px;">'+this.sm_letter+'</p><br>';
+                                html += '  <div class="space20"></div>';
+                                html += ' </div>';
+                                $(".RightSidebar").append($(html))
+
                             })
                         }
                     }
@@ -68,13 +85,100 @@ function loadAboutSchool(id) {
             });
             break;
         case 3:
+            $(function () {
+                $.ajax({
+                    type: "get",
+                    url: "../schoolEnvir",
+                    dataType: "json",
+                    success: function (data) {
+                        if (data.code == 1) {
+                            var info = data.data;
+                            $(".RightSidebar").empty();
+                            $(info).each(function (){
+                                var html = ' <div class="current">';
+                                html += ' <h1>学校环境<em>School Envir</em></h1>';
+                                html += ' <dl><a href="#">首页</a> > <a href="#">学校概况</a> > <span>学校环境</span></dl>';
+                                html += '  </div>';
+                                html += '  <div class="MainContent">';
+                                html += '    <ul class="News_list">';
+                                html += '     <li class="top">';
+                                html += '       <p class="pic"><img src="../images/Pic22.jpg" /></p>';
+                                html += '       <h5>'+this.se_title+'</em></h5>';
+                                html += '     </li>';
+                                html += '    </ul>';
+                                html += '  <div class="space20"></div>';
+                                html += ' </div>';
+                                $(".RightSidebar").append($(html))
+
+                            })
+                        }
+                    }
+                })
+            });
             break;
         case 4:
+            $(function () {
+                $.ajax({
+                    type: "get",
+                    url: "../schoolHonor",
+                    dataType: "json",
+                    success: function (data) {
+                        if (data.code == 1) {
+                            var info = data.data;
+                            $(".RightSidebar").empty();
+                            $(info).each(function (){
+                                var html = ' <div class="current">';
+                                html += ' <h1>学校荣誉<em>School Honor</em></h1>';
+                                html += ' <dl><a href="#">首页</a> > <a href="#">学校概况</a> > <span>学校荣誉</span></dl>';
+                                html += '  </div>';
+                                html += '  <div class="MainContent">';
+                                html += '    <ul class="News_list">';
+                                html += '     <li class="top">';
+                                html += '       <div class="pic"><img src="../images/Pic26.jpg" /></div>';
+                                html += '       <h5>'+this.sh_title+'</h5>';
+                                html += '     </li>';
+                                html += '    </ul>';
+                                html += '  <div class="space20"></div>';
+                                html += ' </div>';
+                                $(".RightSidebar").append($(html))
+                            })
+                        }
+                    }
+                })
+            });
             break;
         case 5:
+            $(function () {
+                $.ajax({
+                    type: "get",
+                    url: "../schoolNews",
+                    dataType: "json",
+                    success: function (data) {
+                        if (data.code == 1) {
+                            var info = data.data;
+                            $(".RightSidebar").empty();
+                            $(info).each(function (){
+                                var html = ' <div class="current">';
+                                html += ' <h1>学校快讯<em>School News</em></h1>';
+                                html += ' <dl><a href="#">首页</a> > <a href="#">学校概况</a> > <span>学校快讯</span></dl>';
+                                html += '  </div>';
+                                html += '  <div class="MainContent">';
+                                html += '    <ul class="News_list">';
+                                html += '     <li class="top">';
+                                html += '       <div class="pic"><img src="../images/Pic23.jpg" /></div>';
+                                html += '       <h5>'+this.sn_title+'<em>发布时间：'+this.sn_pushtime+'</em>'+this.sn_detail+'</h5>';
+                                html += '     </li>';
+                                html += '    </ul>';
+                                html += '  <div class="space20"></div>';
+                                html += ' </div>';
+                                $(".RightSidebar").append($(html))
+
+                            })
+                        }
+                    }
+                })
+            });
             break;
-
-
     }
 }
 
