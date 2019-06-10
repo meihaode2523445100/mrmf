01fb70aa5fa34f8dd6d9aa55cb23209bd94cd5f3
tang <2523445100@qq.com>
2019-06-10 08:59:47 +0800

    tang04 6.10 9:00

--- src/main/java/com/qf/mrmf/service/ProductionService.java
+++ src/main/java/com/qf/mrmf/service/ProductionService.java
@@ -2,10 +2,8 @@ package com.qf.mrmf.service;
 
 import com.qf.mrmf.vo.R;
 
-import java.util.Map;
-
 public interface ProductionService {
     R queryAll();
 
-    Map<String, Object> findByPageOrId(int pr_type_id, int page);
+    R findByPageOrId(int pr_type_id, int page);
 }
