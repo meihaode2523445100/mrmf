f9aab832d0414e963cb4d12048a26ac65220f343
tang <2523445100@qq.com>
2019-06-10 22:49:12 +0800

    tang10 6.10 22:39

--- src/main/resources/static/js/prController.js
+++ src/main/resources/static/js/prController.js
@@ -3,17 +3,14 @@ $(function () {
         type:"get",
         url:"../productionType/all.do",
         dataType:"json",
-        success:function(data) {
+        success:function (data) {
             if (data.code == 1) {
                 var info = data.data;
                 $(info).each(function (){
-                    var html = '<li>';
-                    html +=    '<a href="'+this.pr_type_url+'">'+ this.pr_type+'</a>';
-                    html +=    '</li>';
-                    $("#sc1,#sc2,#aboutProduct,#aboutProduct1").append($(html))
+                    var html = '<li id="menu1"><a href="javascript:findProductIdAndPage('+this.pr_type_id+', 1);" >'+this.pr_type+'</a></li>';
+                    $("#productType,#titleId,#titleId2").append($(html))
                 })
-
             }
         }
     })
-});
+})
